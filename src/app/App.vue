161786b666171351968626e5207839f5e535ec6b
David Eliauri <eliauri.david@gmail.com>
2024-12-11 16:08:09 +0300

    removed shared barrel file

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import { computed } from 'vue';
     import { RouterView, useRoute } from 'vue-router';
-    import { DefaultLayout } from '@/shared';
+    import { DefaultLayout } from '@/shared/ui/';
 
     const route = useRoute();
 
